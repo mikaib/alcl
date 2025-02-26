04d91f469dcc921b1e035360e4abaac87933dd07
http80 <70071615+http-80@users.noreply.github.com>
2025-02-26 15:59:18 +0100

    test linux

--- Scripts/git_test_linux.sh
+++ Scripts/git_test_linux.sh
@@ -6,7 +6,7 @@ TEST_FILES=$(find ./Tests -type f)
 echo "Test files:"
 
 for file in $TEST_FILES do
-    echo $file
+    echo "$file"
 done
 
 ./ALCL -cwd ./Tests -output ./Env/Out/Tests $TEST_FILES
