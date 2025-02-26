aa4efdcde686150ce32833139c45a517df5c1422
http80 <70071615+http-80@users.noreply.github.com>
2025-02-26 22:05:14 +0100

    windows test fix?

--- Scripts/git_test_windows.bat
+++ Scripts/git_test_windows.bat
@@ -3,19 +3,18 @@ echo Running tests...
 echo Current directory: %CD%
 
 cd /d Tests
-(for /r %%F in (*) do @echo %%~nxF) > test_files.txt
-cd /d ..
-
 setlocal enabledelayedexpansion
 set TEST_FILES=
-for /f "delims=" %%F in (Tests\test_files.txt) do (
-    set TEST_FILES=!TEST_FILES! %%F
+for /r %%F in (*) do (
+    set FILE=%%~nxF
+    set TEST_FILES=!TEST_FILES! !FILE!
 )
+cd /d ..
 
 echo Test files:
 for %%F in (!TEST_FILES!) do echo - %%F
 
-./ALCL.exe -cwd ./Tests -compile cmake -std ../Stdlib -output ../Env/Out/Tests !TEST_FILES!
+ALCL -cwd ./Tests -compile cmake -std ../Stdlib -output ../Env/Out/Tests !TEST_FILES!
 echo Tests generated!
 
 endlocal
