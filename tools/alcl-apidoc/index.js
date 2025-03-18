a4226f119f955a19e9743d1a17f28410434c3378
http80 <70071615+http-80@users.noreply.github.com>
2025-03-18 06:53:00 +0100

    progress

--- tools/alcl-apidoc/index.js
+++ tools/alcl-apidoc/index.js
@@ -126,7 +126,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index', { modules: ast, name: projectName, query: req.query });
